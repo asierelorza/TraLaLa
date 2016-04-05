44e5e61b5435f7592091a074f9bb35220c363f67
axi_e <axi_e@MSI>
2016-04-05 16:13:19 +0200

    Main

---
+++ TraLaLa/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.List;
+
+import javax.jdo.Extent;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
