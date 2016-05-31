1752a707c8f5a77dc76f9ae70806b2c44eb41f7c
axi_e <axi_e@MSI>
2016-05-31 08:21:28 +0200

    Eliminación de las interfaces de los services

--- TraLaLa/src/remote/ICancionService.java
+++
@@ -1,9 +0,0 @@
-package remote;
-
-import data.Cancion;
-
-public interface ICancionService {
-	
-	public Cancion getCancion(int id_cancion);
-
-}
--- TraLaLa/src/remote/IUsuarioService.java
+++
@@ -1,7 +0,0 @@
-package remote;
-
-public interface IUsuarioService {
-	
-	public String registrarUsuario(String nombre_usu, String usuario_paypal, String contra_paypal, int numTarjCred, boolean ventajoso, int importeMensual);
-
-}
