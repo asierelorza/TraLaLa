791f7d25398327dcbe5db1ccca580f5378adfdc6
axi_e <axi_e@MSI>
2016-05-31 10:06:18 +0200

    Modificacion del build, inclusion del main

---
+++ TraLaLa/src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+
+import remote.GlobalService;
+import remote.IGlobalService;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];		
+
+		try {
+			IGlobalService remoteFaçade = GlobalService.getInstance();
+
+			Naming.rebind(name, remoteFaçade);
+			System.out.println(" * Auction server '" + name + "' started!!");
+		} catch (Exception ex) {
+			System.err.println(" # Auction Server Exception: " + ex.getMessage());
+		}
+	}
+
+}
